2f6e70fc3280b95a83929580ff149165e65f2984
chdattat <chillal.dattatreya@capgemini.com>
2020-10-02 22:52:02 +0530

    Clean up code

--- src/app/core/components/core.module.ts
+++ src/app/core/components/core.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatCardModule } from '@angular/material/card';
 import { FooterComponent } from './footer/footer.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
@@ -20,13 +15,8 @@ import { RouterModule } from '@angular/router';
   declarations: [LayoutComponent, HeaderComponent, FooterComponent],
   imports: [
     CommonModule,
-    MatSidenavModule,
-    MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatListModule,
-    MatMenuModule,
-    MatCardModule,
     FormsModule,
     ReactiveFormsModule,
     MatInputModule,
@@ -35,13 +25,8 @@ import { RouterModule } from '@angular/router';
     RouterModule,
   ],
   exports: [
-    MatSidenavModule,
-    MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatListModule,
-    MatMenuModule,
-    MatCardModule,
     FormsModule,
     ReactiveFormsModule,
     MatInputModule,
--- src/app/mock-win/components/login/login.component.ts
+++ src/app/mock-win/components/login/login.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class LoginComponent {
+  loginForm: FormGroup;
+  constructor() {
+    this.loginForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.email]),
+      confirm: new FormControl(''),
+    });
   }
-
 }
